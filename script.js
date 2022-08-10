56779216961a1f909a3354fe9669d800f780cfa6
nico martinez <divnicokovacdiv@gmail.com>
2022-08-09 23:58:07 -0400

    first commit

--- script.js
+++ script.js
@@ -33,29 +33,68 @@ const PRODUCTOS_NOTART =[
     }
 
 ];
-
+/* Propiedades  */ 
 class  Producto{
-    constructor(ide, nombre, precio, descripcion, img, categoria){
+    constructor(id, nombre, precio, descripcion, img, categoria){
         this.id = id;
         this.nombre = nombre;
+        this.descripcion = descripcion;
         this.precio = precio;
         this.img = img;
         this.categoria = categoria;
     }
+    //metodos 
     obtenerInfo(){
         return 'ID: $(this.id) | $(this.nombre) $$(this.precio)';
 
     }
-    obtenerAviso(emoji){
+    obtenerAviso(emoji){ 
         return '${this.nombre} >>>>> valgo solo $${this.precio}${emoji}'; 
     }
 }
-
+// variables
 const carrito = [];
 
+// FUnciones 
+
 const obtenerInfoProductos = (productosArray) => {
-    return productosArray.map( (elemento)=> elemento.ObtenerAviso('#').join('\n'));
+    return productosArray.map( (elemento)=> elemento.obtenerInfo()  ).join('\n');
 }
+const agregarAlcarritoPorId = (productos) => {
+    const infoProductos = obtenerInfoProductos(productos);
+    const id = prompt ("ingrese el id del producto que desea agregar:\n "+ infoProductos);
+    const producto = productos.find((producto) => producto.id === id);
+
+    if(!producto) return;
+    carrito.push(producto);
+    alert ("producto agregado al carrito");
+}
+const imprimirCarrito = (carritoDeproductos) => {
+    carritoDeproductos.forEach ((producto) =>{  // para recorrer los productos 
+        console.log(producto.obtenerAviso());
+    });
+}
+
+// Ejecucion, metodo .map 
+
+const productos = PRODUCTOS_NOTART.map (producto => new Producto(
+    producto.id,
+    producto.nombre,
+    producto.precio,
+    producto.descripcion,
+    producto.img,
+    producto.categoria
+));
+
+agregarAlcarritoPorId(productos);
+agregarAlcarritoPorId(productos);
+agregarAlcarritoPorId(productos);
+
+alert ("Abre la consola");
+imprimirCarrito(carrito);
+console.log(obtenerTotal(carrito));
+
+
 
 
 /*
@@ -64,6 +103,21 @@ inputText.addEventListener("input", () => {
 })
 
 09-08
-  cabmsiojsd
+ para formulario 
+idForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const username = document.getElementById("username").value
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+
+    const user = new User(username, email, password)
+
+    users.push(user)
+
+    idForm.reset()
+    console.log(users)
+})
+
+
 
 */
