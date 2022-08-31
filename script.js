bd605c0d2c0db06aee91c908c419c311208bf973
nico martinez <divnicokovacdiv@gmail.com>
2022-08-30 23:42:56 -0400

    first commit

--- script.js
+++ script.js
@@ -5,8 +5,41 @@ class Producto {
     }
 }
 
+/*
+let darkMode
+
+if(localStorage.getItem("darkMode")) { //null si no existe la key
+    darkMode = localStorage.getItem("darkMode") //consulto el local storage
+} else {
+    localStorage.setItem("darkMode", "light") //Creo
+}
+
+if(darkMode == "dark") {
+    document.body.classList.add('darkMode')
+}
+
+const botonDarkMode = document.getElementById("botonDarkMode")
+const botonLightMode = document.getElementById("botonLightMode")
+const botonProductos = document.getElementById("botonProductos")
+const divProductos = document.getElementById("divProductos")
+
+botonDarkMode.addEventListener('click', () => {
+   // document.body.style.backgroundColor = "black"
+   // document.body.style.color = "white"
+   document.body.classList.add('darkMode')
+   localStorage.setItem("darkMode", "dark")
+})
+
+botonLightMode.addEventListener('click', () => {
+   // document.body.style.backgroundColor = "white"
+   // document.body.style.color = "black"
+   document.body.classList.remove('darkMode')
+   localStorage.setItem("darkMode", "light")
+})
 // Stock 
-const contenedorProductos = document.getElementById("contenedor -productos")
+*/
+
+const contenedorProductos = document.getElementById("contenedor-productos")
 
 let carrito = []
 
@@ -27,19 +60,20 @@ productosStock.forEach((producto) =>{
     `
     contenedorProductos.appendChild(div)
 })
+
+const agregarAlcarrito = (proID) => {
+    const item = productosStock.find((prod) =>  prod.id === prodID)
+    carrito.push(item)
+}
+
+
+// Stock
 let productosStock = [
     {id: 1, nombre:" Producto 1", cantidad: 1, descripcion: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto", precio: 12000, talla: "XL", img: "#", tipo: "TipoA",},
     {id: 1, nombre:" Producto 1", cantidad: 1, descripcion: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto", precio: 12000, talla: "XL", img: "#", tipo: "TipoA",},
     {id: 1, nombre:" Producto 1", cantidad: 1, descripcion: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto", precio: 12000, talla: "XL", img: "#", tipo: "TipoA",},
     {id: 1, nombre:" Producto 1", cantidad: 1, descripcion: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto", precio: 12000, talla: "XL", img: "#", tipo: "TipoA",},
     {id: 1, nombre:" Producto 1", cantidad: 1, descripcion: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto", precio: 12000, talla: "XL", img: "#", tipo: "TipoA",},
-    {id: 1, nombre:" Producto 1", cantidad: 1, descripcion: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto", precio: 12000, talla: "XL", img: "#", tipo: "TipoA",},
-    {id: 1, nombre:" Producto 1", cantidad: 1, descripcion: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto", precio: 12000, talla: "XL", img: "#", tipo: "TipoA",},
-    {id: 1, nombre:" Producto 1", cantidad: 1, descripcion: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto", precio: 12000, talla: "XL", img: "#", tipo: "TipoA",},
-    {id: 1, nombre:" Producto 1", cantidad: 1, descripcion: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto", precio: 12000, talla: "XL", img: "#", tipo: "TipoA",},
-    {id: 1, nombre:" Producto 1", cantidad: 1, descripcion: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto", precio: 12000, talla: "XL", img: "#", tipo: "TipoA",},
-    ,{id: 1, nombre:" Producto 1", cantidad: 1, descripcion: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto", precio: 12000, talla: "XL", img: "#", tipo: "TipoA",},
-    {id: 1, nombre:" Producto 1", cantidad: 1, descripcion: "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto", precio: 12000, talla: "XL", img: "#", tipo: "TipoA",}
-    
+   
 
 ]
