25b62d75e9e7fa5261466012159d2c92e8a69ef3
nico martinez <divnicokovacdiv@gmail.com>
2022-09-02 14:05:07 -0400

    first commit

--- script.js
+++ script.js
@@ -9,6 +9,59 @@ class Producto {
     }
 }
 
+//Agregue un logeo , pero no se si podra poner un sweet alert dentro de otro?
+
+
+const botonLogin = document.getElementById("login")
+
+botonLogin.addEventListener("click",() => {
+    Swal.fire({
+        title: 'Login',
+        html:
+          'Usuario<input id="swal-input1" class="swal2-input">' +
+          'Contraseña<input id="swal-input2" class="swal2-input">',
+        focusConfirm: false,
+        preConfirm: () => {
+          return [
+            document.getElementById('swal-input1').value,
+            document.getElementById('swal-input2').value
+          ]
+        }
+      })
+})
+
+// en cada caja de producto tiene un corazon para agregar a favoritos
+
+const botonAgregarFavoritos = document.getElementById("agregarFavoritos")
+
+botonAgregarFavoritos.addEventListener("click",() => {
+    Swal.fire({
+        icon: 'success',
+        title: 'Agregado a favoritos',
+        text: 'Dale click en favoritos para ver todos...',
+        footer: '<a href="#">ir a carrito</a>'
+      })
+})
+
+// Ingrese un input de correo con sweetalert2
+
+const botonEnviarCorreo = document.getElementById("enviarEmail")
+
+botonEnviarCorreo.addEventListener("click",() =>{ 
+        Swal.fire({
+        title: 'Ingrese su correo!',
+        input: 'email',
+        inputLabel: 'Conoce descuentos y noticias',
+        inputPlaceholder: 'Ingrese su correo'
+      })
+      
+      if (email) {
+        Swal.fire(`Ingrese su Correo: ${email}`)
+      }
+    
+})
+
+
 /*
 let darkMode
 
@@ -84,10 +137,3 @@ let productosStock = [
 ]
 
 //libreria
-Swal.fire({
-    position: 'top-end',
-    icon: 'success',
-    title: 'Agregado Correctamente',
-    showConfirmButton: true,
-    timer: 1500
-  })
