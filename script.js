ad1ab56d1986bed5e2230dfe15b94794022ddfbd
nico martinez <divnicokovacdiv@gmail.com>
2022-09-08 23:00:56 -0300

    11

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 
 /*----fetch local*/
+//fetch('./json/productos.json')
+//.then(response => response.json())
+//.then(productos => {
+    
+
+//})
+
 
 const divProductos = document.getElementById("divProductos")
+const botonProductos = document.getElementById("botonProductos")
+const inputProducto = document.getElementById("inputProducto")
 
-const cargarProductos = document.getElementById("")
+const traerProductos = async () => {
+    const response = await fetch('./json/productos.json')
+    const productos = await response.json()
+    return productos
+}
 
-fetch('./json/productos.json')
-.then(response => response.json())
-.then(productos => {
-    productos.forEach((producto, indice) => {
+function mostrarProductos(arraysProductos) {
+    arraysProductos.forEach((producto, indice) => {
         divProductos.innerHTML += `
             <div class="card" id="producto${indice}" style="width: 18rem;top:18 rem;margin:5px;p">
             <img src="./img/${producto.img}" class="card-img-top" alt="...">
@@ -18,12 +29,30 @@ fetch('./json/productos.json')
                     <p class="card-text">Precio:${producto.precio}</p>
                     <p class="card-text">Stock: ${producto.stock}</p>
                 <button id="AgregarAlCarrito" class=" btn btn-dark">Agregar al carrito</button>
+                
                 <button id="AgregarAFavoritos" class=" btn btn-dark"> A Favoritos</button>
+                
             </div>
         </div>
        ` 
     });
 
+}
+
+inputProducto.addEventListener('input', () =>{
+    let res = inputProducto.value
+
+    traerProductos().then(productos =>{
+        const productosFiltrados = productos.filter(producto => producto.nombre.toLowerCase().includes(res.toLowerCase()))
+        divProductos.innerHTML =""
+        mostrarProductos(productosFiltrados)
+    }) 
+})
+
+botonProductos.addEventListener('click', () =>{
+    traerProductos().then(productos => {
+        mostrarProductos(productos)
+    })
 })
 
 
