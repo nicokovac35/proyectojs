343570105e7f76d72e5a885d6f973c5a2b203aa3
nico martinez <divnicokovacdiv@gmail.com>
2022-10-11 12:33:37 -0300

    Proyecto Codehouse JS Nicolas Fuentes

--- js/wincart.js
+++ js/wincart.js
@@ -13,3 +13,39 @@ carritoIcono.onclick = () =>{
 };
 
 //Cerrar ventana de carrito
+
+carritoCerrar.onclick = () =>{
+    carrito.classList.remove("active");
+}
+
+
+//Carrito trabajando con js para
+
+
+if (document.readyState == "loading"){
+    document.addEventListener("DOMContentLoaded", ready);
+
+} else {
+    ready();
+}
+
+
+// Creando funciones para
+function ready(){
+    //remover itens desde el carrito
+    const removerCarritoBoton = document.getElementsByClassName('.cart-remove')
+        for (let i = 0; i < removerCarritoBoton.length; i++){
+            const button = removerCarritoBoton [i];
+            button.addEventListener("click", removerCarritoItem);
+        }
+}
+
+// remover items desde el carrito del
+function removerCarritoItem(event){
+    const buttonClicked = event.target;
+    buttonClicked.parentElement.remove();
+}
+
+//
+
+
