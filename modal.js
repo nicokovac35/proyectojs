ce3ca029b380fbb99da68cdec3b5715a2e436fcf
nico martinez <divnicokovacdiv@gmail.com>
2022-10-15 17:34:23 -0300

    Proyecto final final de finales, Nicolas Fuentes

--- modal.js
+++ modal.js
@@ -17,4 +17,21 @@ contenedorModal.addEventListener('click', (event) =>{
 })
 modalCarrito.addEventListener('click', (event) => {
     event.stopPropagation() 
-})
+});
+
+
+// Para cerrar el banner en la x
+
+const popup =document.querySelector(".bannerfinal");
+const closePopup =document.querySelector(".bannerfinal-close");
+
+if(popup) {
+    closePopup.addEventListener("click", () =>{
+        popup.classList.add("hide-popup");
+    });
+    window.addEventListener("load", ()=>{
+        setTimeout(()=>{
+            popup.classList.remove("hide.popup");
+        }, 1000);
+    });
+};
